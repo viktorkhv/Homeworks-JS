90bf45f1d42e4e9e115b6382a3b44cba44d2dc14
Viktor Khvesiuk <v.berk98@gmail.com>
2020-04-06 01:02:33 +0300

    Homework 15

---
+++ Homework15/Task1/script.js
@@ -0,0 +1,48 @@
+var container = document.getElementById('container');
+
+var firstPar = document.createElement('p'),
+    secondPar = document.createElement('p');
+
+firstPar.innerHTML = 'Hello, here are <a href="http://google.by">Link 1</a> and <a href="http://google.by">Link 2</a>';
+secondPar.innerHTML = 'Hello, here are <a href="http://google.by">Link 3</a> and <a href="http://google.by">Link 4</a>';
+
+container.appendChild(firstPar);
+container.appendChild(secondPar);
+
+var butt = document.getElementsByTagName('button')[0];
+
+butt.onclick = function() {
+    for (var i=0; i < firstPar.children.length; i++) {
+        firstPar.children[i].classList.toggle('red-and-bold');
+    }
+};
+
+localStorage.clear();
+
+secondPar.onclick = function(event) {
+    event.preventDefault();
+    var target = event.target;
+    if (target.tagName != 'a') return;
+    if (localStorage.getItem(target.innerHTML)) {
+        alert(localStorage.getItem(target.innerHTML));
+    } else {
+        localStorage.setItem(target.innerHTML, JSON.stringify({path: target.getAttribute('href')}));
+        alert('Ссылка была сохранена!');
+    }
+}
+for (var j = 0; j < secondPar.children.length; j++) {
+    var link = secondPar.children[j];
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        var target = event.target;
+        if (localStorage.getItem(target.innerHTML)) {
+            alert(localStorage.getItem(target.innerHTML));
+        } else {
+            localStorage.setItem(target.innerHTML, JSON.stringify({path: target.getAttribute('href')}));
+            alert('Ссылка была сохранена!');
+        }
+
+    }, false);
+}
+
+
---
+++ Homework15/Task2/script.js
@@ -0,0 +1,84 @@
+var x = document.getElementById('x'),
+    y = document.getElementById('y'),
+    button = document.getElementById('button');
+
+var valX,
+    valY;
+
+button.setAttribute('disabled', 1);
+x.onkeyup = function () {
+    valX = +x.value;
+    button.removeAttribute('disabled', 1);
+    if (!valY) {
+        button.setAttribute('disabled', 1);
+    }
+}
+
+y.onkeyup = function () {
+    valY = +y.value;
+    button.removeAttribute('disabled', 1);
+    if (!valX) button.setAttribute('disabled',1);
+}
+button.addEventListener('click', buttonEvent);
+function buttonEvent() {
+    destroyTable();
+   var table = document.createElement('table');
+    if (valX && valY && !isNaN(valX) && Number.isInteger(valX) && valX >= 1 && valX <= 10) {
+        if (valY && !isNaN(valY) && Number.isInteger(valY) && valY >= 1 && valY <= 10) {
+            button.setAttribute('disabled',1);
+            document.querySelector('body').appendChild(table);
+            for (var i=0; i<valY; i++) {
+                var tr = document.createElement('tr');
+                for (var j=0; j<valX; j++) {
+                    table.appendChild(tr);
+                    var td = document.createElement('td');
+                    tr.appendChild(td);
+                }
+            }
+        }
+
+    } else {
+        alert('введите корректные числовые значения от 1 до 10');
+        x.value = '';
+        y.value = '';
+    }
+    paintTab();
+
+    document.querySelector('table').addEventListener('click', function () {
+        if
+            changeColors();
+        } else {
+            paintTab();
+        }
+    });
+}
+
+function destroyTable() {
+    if (document.querySelector('table') != null) {
+        document.querySelector('body').removeChild(document.querySelector('table'));
+        x.value = '';
+        y.value = '';
+    }
+}
+
+function paintTab() {
+    obj = document.querySelector('table');
+    for (y = 0; y <obj.rows.length; y++) {
+        for (x = 0; x <obj.rows[0].cells.length; x++) {
+
+            if((x + y) % 2 ) {
+                obj.rows[y].cells[x].className = "black";}}};
+
+}
+function changeColors() {
+    var x, y;
+    obj = document.querySelector('table');
+    for (y = 0; y <obj.rows.length; y++) {
+        for (x = 0; x <obj.rows[0].cells.length; x++) {
+            obj.rows[y].cells[x].className = "white";
+            if(!((x + y) % 2) ) {
+                obj.rows[y].cells[x].className = "black";}}};
+
+}
+
+
